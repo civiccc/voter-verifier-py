42ff1605bbe104018bf110a277465dff3f421ca2
Tom Dooner <tom.dooner@brigade.com>
2014-12-04 19:00:50 -0500

    Memoize validator instance across requests
    Some Gerrit feedback from Noah prompts this. There's also some other
    changes in here that he suggests, but nothing too dramatic.
    
    Change-Id: Iacae69df4ade459049d4901e421c6f11402a37ad

--- web.py
+++ web.py
@@ -6,37 +6,41 @@ from jsonschema import Draft4Validator
 ***REMOVED***
 
 app = Flask(__name__)
+validator = None
 
 
 def validate_api_request(response_continuation):
+    global validator
+    if not validator:
+        validator = Draft4Validator(json.loads(open('schema.json', 'r').read()))
+
     @wraps(response_continuation)
     def with_validation():
-        schema = json.loads(open('schema.json', 'r').read())
         method = request.method
         path = request.path
-        params = request.get_json()
 
-        request_schema = None
-        for chunk_name, chunk in schema["definitions"].items():
+        for chunk in validator.schema["definitions"].values():
             if "links" not in chunk:
                 continue
-            for endpoint in chunk["links"]:
-                if endpoint["href"] == path and endpoint["method"] == method:
-                    request_schema = endpoint["schema"]
+            link_matches = lambda l: l["href"] == path and l["method"] == method
+            request_schema = next((l for l in chunk["links"] if link_matches(l)), {}).get('schema')
+
+        params = request.get_json()
 
         if request_schema is None:
             print "Couldn't find request schema for {0} {1}".format(method, path)
-            return response_continuation(params)
+            return response_continuation()
 
-        errors = list(Draft4Validator(schema).iter_errors(params, request_schema))
+        errors = list(validator.iter_errors(params, request_schema))
 
         if not errors:
             return response_continuation(params)
-        else:
-            error_str = json.dumps([{
+
+        return json.dumps([
+            {
                 "message": error.message,
-                "context": ".".join(error.absolute_path)} for error in errors])
-            return error_str, 422
+                "context": ".".join(error.absolute_path)
+            } for error in errors]), 422
 
     return with_validation
 
@@ -53,15 +57,23 @@ def home():
     """
 
 
-@app.route('/v1/voters/info', methods=['POST'])
-@validate_api_request
-def match(valid_api_params):
-    return str(valid_api_params['id'])
+@app.route('/v1/voters/<id>', methods=['GET'])
+def find_voter(id):
+    """
+    Find a voter by voterbase ID.
+
+    TODO: Implement this.
+    """
+    return str(id)
 
 
 @app.route('/v1/voters/search', methods=['POST'])
 @validate_api_request
 def search(valid_api_params):
+    """
+    Match a chunk of user-inputted information (name, address, dob, etc.) to an
+    entry in a voter roll.
+    """
     res = match_one(**valid_api_params['user'])
 
     return str(res)
