a9da44304a8c95adcc15de42c1fe9f4dddbfb971
Tom Dooner <tom.dooner@brigade.com>
2015-10-13 23:07:17 +0000

    Replace S3 with HDFS in `list_files.py`
    Now that we have a copy of the targetsmart data in HDFS, we should stop
    incurring the bandwidth waste and cost of data transfer out of Amazon
    whenever we want to reindex this data.
    
    This uses the HDFS over HTTP application we set up for Hue. In the
    future we will probably want to add security to it (as right now it is
    unauthenticated), and so I added a bit of error checking into this
    script so that it breaks with a good error message in that case.
    
    Change-Id: Iea61b41af40f3e3e3d78dbc69dcf3a99c13ffa1c
    Reviewed-on: https://code.brigade.com/6436
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>
    Tested-by: Tom Dooner <tom.dooner@brigade.com>

--- list_files.py
+++ list_files.py
@@ -1,16 +1,33 @@
-# Make sure you have aws env variables (AWS_ACCESS_KEY_ID and
-# AWS_SECRET_ACCESS_KEY)
-import boto
-
-FOLDER_NAME = '20150608analysis/'
-# How long the download link is valid for before it expires
-URL_VALID_TIME = 43200 # 12 hours
-
-if __name__ == '__main__':
-    conn = boto.connect_s3()
+# This gets file URLs from HDFS
+import requests
+import os
+import sys
 
 ***REMOVED***
-        # Print all files except for the folder itself
-        if key.name == FOLDER_NAME: continue
+# Since we have security turned off on webhdfs, we just need some value here.
+# It can be anything, but not "verifier" since that user has write permissions
+# and we don't want to accidentally delete files later.
+USER='verifier_download'
 
-        print key.generate_url(URL_VALID_TIME, query_auth=True)
+def get_filenames():
+***REMOVED***
+
+    if resp.status_code != 200:
+        os.stderr.write("Error fetching list of files from HDFS gateway!")
+        os.stderr.write("This probably means we have enabled authentication on HDFS via HTTP")
+        os.stderr.write("(TODO: Rewrite this script to support that)")
+        os.stderr.write(resp.text)
+        sys.exit(1)
+
+    for f in resp.json()['FileStatuses']['FileStatus']:
+        if f['type'] != 'FILE':
+            continue
+
+        name = f['pathSuffix']
+
+***REMOVED***
+        yield url
+
+if __name__ == '__main__':
+    for filename in get_filenames():
+        print filename
