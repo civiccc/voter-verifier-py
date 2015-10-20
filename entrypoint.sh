c9193116afe7aa752e3345e9a0b4a23d4922f251
Tom Dooner <tom.dooner@brigade.com>
2015-10-20 04:13:05 +0000

    Finish verifier config for bli-based-deploy
    With this config, and minor adjustment to the entrypoint, `bli deploy`
    is capable of deploying the verifier.
    
    This is a bit different than in Ruby because virtualenv puts the full
    path in the shims' shebangs, so we need to make sure the app is always
    run out of /app. We can accomplish this by doing a simple check of path
    and setting up a symlink if necessary.
    
    Change-Id: Idf5445d3df5e0e6065e6a265ec7b18f77f0073f8
    Reviewed-on: https://code.brigade.com/6724
    Tested-by: Leeroy Jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-set -euo pipefail
+set -exuo pipefail
 
-mkdir -p ${APP_DIRECTORY:-/app}
-chown $UID:$GID ${APP_DIRECTORY:-/app}
+if [ ${APP_DIRECTORY} != "/app" ]; then
+  rm -rf /app
+  ln -sfT $APP_DIRECTORY /app
+fi
+
+chown $UID:$GID /app
 
 # TODO: do this conditionally if they are defined, and make sure you can't set
 # UID=1.
@@ -13,5 +17,5 @@ usermod --non-unique -u $UID -g $GID app
 docker_host=$(ip route | grep default | awk '{ print $3 }')
 echo "${docker_host} docker-host" >> /etc/hosts
 
-cd ${APP_DIRECTORY:-/app}
+cd /app
 gosu app "$@"
